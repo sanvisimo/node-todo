a38a0fecb9942f4aca3809f4672a0148a8c364ce
Simone Sanvito <sanvi.simo@gmail.com>
2018-04-14 15:48:50 +0200

    add login

--- server/models/user.js
+++ server/models/user.js
@@ -67,6 +67,30 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    let User = this;
+
+    return User.findOne({email}).then((user) => {
+      if(!user) {
+          return Promise.reject('email non riconosciuta');
+      }
+
+      return new Promise((resolve, reject) => {
+          bcrypt.compare(password, user.password, (err, res) => {
+              if(res){
+                  resolve(user);
+              } else {
+                  reject('Password sbagliata');
+              }
+
+              if(err) {
+                  reject(err);
+              }
+          });
+      });
+    });
+};
+
 UserSchema.pre('save', function(next) {
   let user = this;
   if(user.isModified('password')) {
